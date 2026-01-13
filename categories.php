2f407be29cf7a1e0a60098cab349e35a50eb5f2b
faizayousuf123 <faizayousuf29@gmail.com>
2026-01-12 16:35:32 -0800

    Add categories.php page

---
+++ categories.php
@@ -0,0 +1,53 @@
+<?php
+include('config.php');
+
+
+include('./functions/collectionlogic.php');
+
+?>
+<div class="py-3 bg-primary">
+    <div class="container">
+        <h6 class="text-white">Home/Collection</h6>
+    </div>
+</div>
+<div class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>OUR COLLECTION</h1>
+                <hr>
+                <div class="row">
+                <?php
+           $categories = getAllactive("categories");
+       if(mysqli_num_rows($categories)>0){
+        foreach($categories as $item)
+        {
+            
+         ?>
+         <div class="col-md-3 mb-2">
+            <div class="card shadow">
+                <div class="card-body">
+  <img src="uploads/<?= $item['image']; ?>" alt="category image" class="w-100">
+  <h4 class="text-center"><?= $item['name']; ?></h4>
+
+            </div>
+         </div>
+
+         </div>
+        
+         <?php
+        }
+       }else{
+         echo"no data found";
+         }
+         ?>
+                </div>
+               
+            </div>
+        </div>
+    </div>
+</div>
+<?php 
+require_once 'includes/header.php';
+include('includes/footer.php');
+?>
